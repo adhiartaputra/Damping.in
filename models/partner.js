794778e1678af2fde824d89f5ffb9211be91800d
adhiarta <adhiartaprabataputra@gmail.com>
2018-02-28 13:26:31 +0700

    Half of crud partner

---
+++ models/partner.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Partner = sequelize.define('Partner', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    height: DataTypes.INTEGER,
+    weight: DataTypes.INTEGER,
+    rate: DataTypes.INTEGER
+  });
+  Partner.associate = function(models) {
+    Partner.belongsToMany(models.User, {
+      through    : 'user_partners',
+      foreignKey : 'partnerId'
+    })
+  };
+  return Partner;
+};
---
+++ models/user_partner.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var user_partner = sequelize.define('user_partner', {
+    partnerId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
+  }, {});
+  user_partner.associate = function(models) {
+    user_partner.belongsTo(models.User, {
+      foreignKey: 'userId'
+    })
+    user_partner.belongsTo(models.Partner, {
+      foreignKey: 'partnerId'
+    })
+  };
+  return user_partner;
+};
